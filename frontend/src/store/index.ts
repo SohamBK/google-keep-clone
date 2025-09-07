7cd303ea1f3e33c092d8572b0fd983b9f1118d53
SohamBK <balekarsoham@gmail.com>
2025-09-08 01:35:15 +0530

    feat:frontend Added notes slice

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import notesReducer from "./notes/notesSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    notes: notesReducer,
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {auth: AuthState}
 export type AppDispatch = typeof store.dispatch;
